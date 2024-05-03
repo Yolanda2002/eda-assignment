b9de044d134bfd9b222b65233aa96336e117daf8
Jia Yang <20104736@mail.wit.ie>
2024-05-03 22:29:59 +0100

    Signup resource

--- bin/eda-app.ts
+++ bin/eda-app.ts
@@ -3,7 +3,7 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { EDAAppStack } from "../lib/eda-app-stack";
 
-const app = new cdk.App();
+const app = new cdk.App();//创建cdk实例
 new EDAAppStack(app, "EDAStack", {
   env: { region: "eu-west-1" },
 });
--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -13,17 +13,58 @@ import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EDAAppStack extends cdk.Stack {
+  // 定义云设施堆栈
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //创建存储桶
     const imagesBucket = new s3.Bucket(this, "images", {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       publicReadAccess: false,
     });
 
+    // Integration infrastructure
+    // SQS队列暂存图片
+    const queue = new sqs.Queue(this, "img-created-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
+
+    // Lambda functions
+
+    const processImageFn = new lambdanode.NodejsFunction(
+      this,
+      "ProcessImageFn",
+      {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambdas/processImage.ts`,
+        timeout: cdk.Duration.seconds(15),
+        memorySize: 128,
+      }
+    );
+
+    // S3 --> SQS
+    //  对象创建时发送提示
+    imagesBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.SqsDestination(queue)
+    );
+
+    // SQS --> Lambda
+    //  触发lambda语句
+    const newImageEventSource = new events.SqsEventSource(queue, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(10),
+    });
+
+    processImageFn.addEventSource(newImageEventSource);
+
+    // Permissions
+    // 权限
+    imagesBucket.grantRead(processImageFn);
+
     // Output
-    
+    //输出桶名称
     new cdk.CfnOutput(this, "bucketName", {
       value: imagesBucket.bucketName,
     });
